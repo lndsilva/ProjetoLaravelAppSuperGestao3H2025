3e8f55d14dd486a8cf3c7bf711a62ed12d38cde2
lndsilva <lndsilva@hotmail.com>
2025-02-25 20:18:35 -0300

    projeto laravel super gestão

---
+++ resources/views/site/contato.blade.php
@@ -0,0 +1,7 @@
+<h3>Contato - (View)</h3>
+
+<ul>
+    <li><a href="{{route('site.principal')}}">Principal</a></li>
+    <li><a href="{{route('site.contato')}}">Contato</a></li>
+    <li><a href="{{route('site.sobre-nos')}}">Sobre nos</a></li>
+</ul>
