d8a311b61445c6a260a63b5f31b18b3275681456
lndsilva <lndsilva@hotmail.com>
2025-02-25 19:54:26 -0300

    projeto laravel super gestão

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/contato', function () {
+    return view('welcome');
+});
+
+Route::get('/sobre-nos', function () {
+    return view('welcome');
+});
+
+Route::get('/login', function () {
+    return view('welcome');
+});
+
+Route::get('/clientes', function () {
+    return view('welcome');
+});
+
+Route::get('/fornecedores', function () {
+    return view('welcome');
+});
+
+Route::get('/produtos', function () {
+    return view('welcome');
+});
